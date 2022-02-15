c9c630df1b4b3832e9f33a920ac3a3031fed7337
tbnsok40 <tbnsok40@gmail.com>
2022-02-15 23:01:39 +0900

    User class 생성

---
+++ src/com/company/v0/WorkPolicy/User.java
@@ -0,0 +1,22 @@
+package com.company.v0.WorkPolicy;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private boolean managerAuthority;
+
+    public User authorizeManager(Long id) {
+        if (authorize(id)) {
+            return manager;
+        } else {
+            return null;
+        }
+    }
+
+
+
+
+
+
+}
