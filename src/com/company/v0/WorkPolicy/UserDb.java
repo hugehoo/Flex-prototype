b84113d0a0a4b47e1b6685972ff6644805218e7b
tbnsok40 <tbnsok40@gmail.com>
2022-02-17 16:37:35 +0900

    UserD class 생성 및 findManagerById() 메서드 작성

---
+++ src/com/company/v0/WorkPolicy/UserDb.java
@@ -0,0 +1,10 @@
+package com.company.v0.WorkPolicy;
+
+// Repository
+public class UserDb {
+
+    public static Boolean findManagerById(Long id) {
+        System.out.println("find Manager");
+        return true;
+    }
+}
