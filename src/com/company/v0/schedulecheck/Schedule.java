4dc321669f45a747ca07b6116cc32bd65ec4753a
tbnsok40 <tbnsok40@gmail.com>
2022-02-21 00:33:42 +0900

    [Schedule] getWorkPolicy(), getAllScheduleByDate() 메서드 생성

--- src/com/company/v0/schedulecheck/Schedule.java
+++ src/com/company/v0/schedulecheck/Schedule.java
@@ -1,5 +1,16 @@
-package com.company.v0.memberschedule;
+package com.company.v0.schedulecheck;
+
+import java.util.List;
 
 public class Schedule {
 
+    private String workPolicy;
+
+    public String getWorkPolicy() {
+        return workPolicy;
+    }
+
+    public static List<Schedule> getAllScheduleByDate(Object dateRange) {
+        System.out.println("모든 멤버의 스케쥴을 dateRange 필터링에 따라 보여준다.");
+    }
 }
