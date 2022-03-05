8bd8ed43dcd3194feeaf25c4358c3b1da5eaff91
hugehoo <tbnsok40@gmail.com>
2022-03-05 16:11:36 +0900

    ScheduleRepository interface: 개인 스케쥴 조회 및 스케쥴 저장 기능

--- app/src/main/java/flex/schduleDayOff/ScheduleRepository.java
+++ app/src/main/java/flex/schduleDayOff/ScheduleRepository.java
@@ -1,2 +1,14 @@
-package flex.schduleDayOff;public interface S {
+package flex.schduleDayOff;
+
+import java.util.Date;
+import java.util.List;
+
+interface ScheduleRepository {
+
+    List<Schedule> getPersonalWeeklySchedule(long userId, Date date);
+
+    void save(List<Schedule> schedules);
+
+    List<Schedule> getAllDayOffSchedule(long userId);
+
 }
