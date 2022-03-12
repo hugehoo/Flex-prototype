ea5511c9d3896cec8b6efcf61f93df55f2a35aff
hugehoo <tbnsok40@gmail.com>
2022-03-12 15:54:46 +0900

    WorkPolicy class : 근무정책 생성, 삭제 기능 구현

---
+++ app/src/main/java/Flex/v1/company/WorkPolicyMain.java
@@ -0,0 +1,20 @@
+package Flex.v1.company;
+
+public class WorkPolicyMain {
+
+    public void execute() {
+
+        WorkPolicy workPolicy1 = new WorkPolicy(
+            "반차",
+            "오전반차 혹은 오후반차",
+            "팀내 공유",
+            true,
+            true,
+            true,
+            true,
+            false
+        );
+
+        workPolicy1.notifyAdministrator();
+    }
+}
