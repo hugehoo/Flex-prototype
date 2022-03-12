422e0a131948c5f5294b6f20a2c89db897d643d0
hugehoo <tbnsok40@gmail.com>
2022-03-12 15:49:52 +0900

    WorkType Class : 근무유형 생성 및 근무시간 계산 담당 클래스 개발

---
+++ app/src/main/java/Flex/v1/company/WorkType.java
@@ -0,0 +1,111 @@
+package Flex.v1.company;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 회사의 근무유형을 생성, 관리하는 클래스
+ */
+public class WorkType {
+
+    /*
+     근무 유형 이름
+     */
+    private String name;
+
+    /*
+     근무 유형 종류 (고정/시차/선택적/스케쥴)
+     */
+    private String workManagement;
+
+    /*
+     출근 시간
+     */
+    private String startHour;
+
+    /*
+     퇴근 시간
+     */
+    private String endHour;
+
+    /*
+     일 휴게 시간
+     */
+    private long lunchBreak;
+
+    /*
+     일하는 날
+     */
+    private List<String> workingDay;
+
+    /*
+     유급 휴일
+     */
+    private List<String> offDay;
+
+    /*
+     법정 근로시간 (주간)
+     */
+    private final int StatutoryWorkingHour = 40;
+
+    public WorkType(String name, String workManagement, String startHour, String endHour,
+        long lunchBreak, List<String> workingDay, List<String> offDay) {
+        this.name = name;
+        this.workManagement = workManagement;
+        this.startHour = startHour;
+        this.endHour = endHour;
+        this.lunchBreak = lunchBreak;
+        this.workingDay = workingDay;
+        this.offDay = offDay;
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(WorkType.class);
+
+    /**
+     * 출퇴근 시간을 계산하여 하루 근무시간을 계산합니다.
+     *
+     * @return 일 근무시간
+     * @throws ParseException
+     */
+    public long countDailyWorkingHour() throws ParseException {
+
+        SimpleDateFormat HHMMSS = new SimpleDateFormat("HH:mm:ss");
+        Date end = HHMMSS.parse(this.endHour);
+        Date start = HHMMSS.parse(this.startHour);
+        long dailyHour = ((end.getTime() - start.getTime()) / 60000 / 60) - this.lunchBreak;
+
+        logger.info("하루 {} 시간 근무 ", dailyHour);
+
+        return dailyHour;
+    }
+
+    /**
+     * 주간 근무시간을 계산합니다.
+     * @return 주 근무시간
+     * @throws ParseException
+     */
+    public int countWeeklyWorkingHour() throws ParseException {
+        int Size = this.workingDay.size();
+        int dailyHour = (int) countDailyWorkingHour();
+
+        logger.info("주 {} 시간 근무 ", Size * dailyHour);
+        return Size * dailyHour;
+    }
+
+    /**
+     * 주간 법정 근로시간을 준수하는지 확인합니다.
+     * @param WeeklyWorkingHour
+     */
+    public void checkStatutoryWorkingHour(int WeeklyWorkingHour){
+        if (StatutoryWorkingHour >= WeeklyWorkingHour) {
+            logger.info("적법한 설정입니다");
+        } else {
+            logger.info("법정 근로시간을 준수해주세요");
+        }
+    }
+}
---
+++ app/src/main/java/Flex/v1/company/WorkTypeMain.java
@@ -0,0 +1,48 @@
+package Flex.v1.company;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkTypeMain {
+    List<String> workingDay;
+    List<String> offDay;
+
+    /**
+     * 일하는 날을 지정합니다.
+     */
+    public void setWorkingDay() {
+        workingDay = new ArrayList<>();
+        workingDay.add("월");
+        workingDay.add("화");
+        workingDay.add("수");
+        workingDay.add("목");
+        workingDay.add("금");
+    }
+
+    /**
+     * 유급 휴일일을 지정합니다.
+     */
+    public void setOffDay() {
+        offDay = new ArrayList<>();
+        offDay.add("일");
+    }
+
+    public void execute() throws ParseException {
+        setOffDay();
+        setWorkingDay();
+        WorkType workType = new WorkType(
+            "고정 출퇴근제",
+            "고정",
+            "09:00:00",
+            "18:00:00",
+            1L,
+            workingDay,
+            offDay
+        );
+
+        int WeeklyHour = workType.countWeeklyWorkingHour();
+        workType.checkStatutoryWorkingHour(WeeklyHour);
+    }
+
+}
