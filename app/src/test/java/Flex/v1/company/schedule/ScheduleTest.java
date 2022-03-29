3572f639cea5b02108993e376e0d8d43515f9391
hugehoo <tbnsok40@gmail.com>
2022-03-29 16:44:43 +0900

    Schedule Test code 작성

--- app/src/test/java/Flex/v1/company/schedule/ScheduleTest.java
+++ app/src/test/java/Flex/v1/company/schedule/ScheduleTest.java
@@ -1,37 +1,23 @@
 package Flex.v1.company.schedule;
 
 import Flex.v1.company.worktype.WorkType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.text.ParseException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ScheduleTest {
-
 
     static WorkType personalWorkType;
     Schedule schedule;
 
     @Test
-    void 근무시간중_초과된_근무시간_계산() throws ParseException {
-
-        schedule = Schedule
-                .builder()
-                .userId(123L)
-                .date("2022-03-28")
-                .workType(personalWorkType)
-                .startTime("09:00:00")
-                .endTime("21:10:00")
-                .lunchBreak(1L)
-                .build();
-
-        assertEquals(190,  schedule.calculateOverWorkHours());
-
-    }
-
-    @Test
-    void 출퇴근시간에따라_하루근무시간_계산() throws ParseException {
+    void Calculate_Overwork_Time() throws ParseException {
         schedule = Schedule
                 .builder()
                 .userId(123L)
@@ -43,4 +29,38 @@ class ScheduleTest {
                 .build();
         assertEquals(430, schedule.calculateWorkHours());
     }
+
+    @Test
+    @DisplayName("출퇴근 시간이 없으면, NullPointerException 을 던진다") // 휴일로 간주하는게 맞을거 같은데
+    void When_No_Start_End_Time_Throws_NPException() throws ParseException {
+
+        Schedule schedule = Schedule
+                .builder()
+                .userId(123L)
+                .date("2022-03-28")
+                .workType(personalWorkType)
+                .build();
+
+        assertEquals(-1L, schedule.calculateWorkHours());
+    }
+
+    @Test
+    @DisplayName("특정 날짜를 휴가로 지정하면, 해당 날짜의 스케쥴은 휴가가 된다.")
+    void When_Set_Leave_Schedule_Changed() {
+        // 이 구문은 어떻게 쓰는거지?
+//        when(scheduleRepository.getUserSchedule(anyInt())).thenReturn(null);
+//        assertNull(scheduleRepository.getUserSchedule(anyInt()));
+
+        Schedule.setLeave("2022-05-19");
+        Schedule schedule = ScheduleRepository.personalScheduleMap.get("2022-05-19");
+        assertEquals("반차", schedule.getWorkType().getName());
+
+    }
+
+    @Test
+    @DisplayName("휴가를 지정할 때 날짜가 없다면, NullPointerException 을 던진다.")
+    void When_Leave_Date_IS_Null_Throws_NPE() {
+        assertThrows(NullPointerException.class, () -> Schedule.setLeave(null));
+    }
+
 }
