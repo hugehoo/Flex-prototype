73f4957ae8853d4e2e9910a6ec7ab1f6b47bf0bd
hugehoo <tbnsok40@gmail.com>
2022-03-29 09:23:03 +0900

    DayOffRepository Test 코드 작성 및 DayOffRepository 클래스 로직 수정

--- app/src/main/java/Flex/v1/company/user/User.java
+++ app/src/main/java/Flex/v1/company/user/User.java
@@ -1,7 +1,11 @@
 package Flex.v1.company.user;
 
+import Flex.v1.company.dayoff.CompanyDayOff;
+import Flex.v1.company.dayoff.DayOffRepository;
 import Flex.v1.company.worktype.WorkType;
 import java.util.Date;
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -49,6 +53,9 @@ public class User {
     private String role;
 
     private Date dateOfEntry;
+
+    static public List<CompanyDayOff> dayOffList = DayOffRepository.getOffDays();
+
 
     /**
      * 확정된 연차 내역
