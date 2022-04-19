8ac9cf900b1a32849f34628cceca4f23f92f15d8
hugehoo <tbnsok40@gmail.com>
2022-04-19 11:11:25 +0900

    test/ Test code 추가

--- app/src/main/java/Flex/v2/repository/ScheduleJpaRepository.java
+++ app/src/main/java/Flex/v2/repository/ScheduleJpaRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface ScheduleJpaRepository extends JpaRepository<Schedule, Long> {
 
     List<Schedule> findScheduleByMember(Member member);
 
-    Schedule findScheduleByMemberAndDate(Member member, LocalDate now);
+    Optional<List<Schedule>> findScheduleByMemberAndDate(Member member, LocalDate date);
 
     List<Schedule> findScheduleByMemberAndDateBetween(Member member, LocalDate from, LocalDate to);
 
